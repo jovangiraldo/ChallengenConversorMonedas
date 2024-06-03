90d686112269edce7c78965eb550c96040947fb8
Jovan <jovancorrea311862@correo.itm.edu.co>
2024-06-02 23:01:42 -0500

    inicio

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double dinero = 0;
+
+
+        dinero = Double.parseDouble(JOptionPane.showInputDialog("Ingresa la  cantidad de dinero que desea convertir"));
+
+
+
+    }
+}
